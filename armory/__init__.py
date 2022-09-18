9330ccf6a0864e6e72c158717d31a89c76718c92
christopherwoodall <woodall.christopher@gmail.com>
2022-09-18 03:54:26 -0400

    update auto versioning

--- armory/__init__.py
+++ armory/__init__.py
@@ -3,6 +3,7 @@ Adversarial Robustness Evaluation Test Bed
 """
 
 from typing import Dict, Any
+from pathlib import Path
 
 from armory.logs import log
 from armory.version import get_version
@@ -12,7 +13,7 @@ __all__ = (
     'armory'
 )
 
-__version__ = "0.15.1"
+__version__ = Path(Path(__file__).parent / 'VERSION').read_text().strip()
 
 
 # typedef for a widely used JSON-like configuration specification
