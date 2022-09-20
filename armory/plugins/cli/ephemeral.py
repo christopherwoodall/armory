f9dac5b062d8a28be795c166bfe03bc9f1b19eba
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 11:44:18 -0400

    work on plugin factory

--- armory/plugins/cli/ephemeral.py
+++ armory/plugins/cli/ephemeral.py
@@ -30,45 +30,61 @@ MIN_PYTHON_VERSION = (3, 7, 0)
 BASE_PATH          = Path.cwd()
 
 
-def get_commands():
-    skip_names = ("__init__.py", "__main__.py", "ephemeral.py")
-    commands      = { }
-    command_files = [f for f in Path(__file__).parent.iterdir() if f.is_file() and f.name not in skip_names]
-    for command in command_files:
-        commands[command.stem] = {
-            "path":   command,
-            "module": f"{command.stem.title()}CLI",
-            "description": "TODO",
+class EphemeralCLI(CLI):
+    def __init__(self, args):
+        self.commands = self.locate_commands()
+        self.args     = self.resolve(args)
+
+
+    def resolve(self, args):
+        if (command := args[1]) not in self.commands.keys():
+            print(f"ERROR: Unknown command {command}", file=sys.stderr)
+            sys.exit(2)
+        return args[1:]
+
+
+    def dispatch(self):
+        command = self.args[0]
+        data    = self.commands[command]
+        module  = self.module_loader(data['module'], data['path'])
+
+        import pprint
+        pp = pprint.PrettyPrinter(indent=2)
+        pp.pprint(getmembers(module))
+
+        module()     # module(self.args.pop())
+        return
+
+
+    def locate_commands(self):
+        skip_names = ("__init__.py", "__main__.py", "ephemeral.py")
+        commands = { f.stem: {
+                "path": f,
+                "name": f.name,
+                "module": f"{f.stem.capitalize()}CLI",
+                "description": "No description available."
+            }
+            for f in Path(__file__).parent.iterdir() if f.is_file() and f.name not in skip_names
         }
-    return commands
+        return commands
 
 
-# @debugging.trace
-def module_loader(module_name: str, filepath: Path) -> class:
-    path      = str(filepath.absolute())
-    filename  = filepath.name
-    name, ext = (filepath.stem, filepath.suffix)
-
-    loader = importlib._bootstrap_external.SourceFileLoader(name, path)
-    spec = importlib.util.spec_from_file_location(name, path, loader=loader)
-
-    try:
-        module = getattr(importlib._bootstrap._load(spec), module_name, False)
-        if module:
-            print(type(module))
-            return module
-        else:
-            raise Exception(f"Error importing {module_name} module from {filename}!")
-    except:
-        raise ImportError(path, sys.exc_info())
-
+    # @debugging.trace
+    def module_loader(self, module_name: str, filepath: Path):
+        path   = str(filepath.absolute())
+        loader = importlib._bootstrap_external.SourceFileLoader(filepath.stem, path)
+        spec   = importlib.util.spec_from_file_location(filepath.stem, path, loader=loader)
+        try:
+            module = getattr(importlib._bootstrap._load(spec), module_name, False)
+            if module:
+                return module
+            else:
+                raise Exception(f"Error importing {module_name} module from {filepath}!")
+        except:
+            raise ImportError(path, sys.exc_info())
 
 
 def main():
-    # Grab a list of commands
-    command_list = get_commands()
-
-    # Check python version
     if version.version_tuple(platform.python_version()) < MIN_PYTHON_VERSION:
         print(f"ERROR: Armory requires Python {'.'.join(map(str, MIN_PYTHON_VERSION))} or higher.")
         sys.exit(1)
@@ -77,13 +93,4 @@ def main():
         print("ERROR: No command given", file=sys.stderr)
         sys.exit(1)
 
-    if (command := sys.argv[1]) not in command_list.keys():
-        print(f"ERROR: Unknown command {command}", file=sys.stderr)
-        sys.exit(2)
-
-
-    # TODO:
-    command_data   = command_list[command]
-    command_module = module_loader(command_data['module'], command_data['path'])
-    command_module()
-
+    return EphemeralCLI(sys.argv).dispatch()
