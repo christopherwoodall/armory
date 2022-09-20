9c62e5ff092cfd0ae7cc668e07ecc2f6197592b0
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 15:59:15 -0400

    remove debug

--- armory/__main__.py
+++ armory/__main__.py
@@ -8,17 +8,17 @@ APP_NAME = 'armory'
 def main():
     dist = distribution(APP_NAME)
     ep_map = {ep.name: ep for ep in dist.entry_points if ep.group == 'console_scripts'}
-    print(ep_map)
+    # print(ep_map)
 
     parser = argparse.ArgumentParser(prog=f"python -m {APP_NAME}", add_help=False)
     parser.add_argument('entry_point', choices=list(ep_map) + ['test'])
     args, extra = parser.parse_known_args()
 
-
     main = ep_map[args.entry_point].load()
 
     # main([args.entry_point] + extra)
     main()
+
 
 if __name__ == '__main__':
     main()
