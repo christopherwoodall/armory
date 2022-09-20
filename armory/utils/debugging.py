70ee6e5bce624868e61744e43601e7d04efd2e02
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 10:20:53 -0400

    use pdb as tracer

--- armory/utils/debugging.py
+++ armory/utils/debugging.py
@@ -17,24 +17,7 @@ def module_path(local_func):
   return os.path.abspath(inspect.getsourcefile(local_func))
 
 
-def trace():
-  ''' Prints the current stack trace.
-
-  Example:
-    >>> tracer = trace()
-    >>> tracer.start()
-  '''
-  def tracer(frame, event, arg = None):
-    code      = frame.f_code
-    func_name = code.co_name
-    line_no   = frame.f_lineno
-    print(f"FUNC:\t{func_name}()\tEVENT: {event}\tLINE: {line_no} ")
-    return tracer
-
-  def start():
-    sys.settrace(tracer)
-
-  def stop():
-    sys.settrace(None)
-
-  return (start, stop)
+def trace(func):
+  def wrapper():
+    import pdb; pdb.set_trace()
+  return wrapper
