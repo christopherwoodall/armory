6da5a1c7885695155871b42899b4cd20ad7143e5
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 15:56:21 -0400

    compat

--- armory/__main__.py
+++ armory/__main__.py
@@ -8,6 +8,10 @@ def main():
     ep_map = {ep.name: ep for ep in dist.entry_points if ep.group == 'console_scripts'}
     print(ep_map)
 
+    parser = argparse.ArgumentParser(prog=f"python -m {APP_NAME}", add_help=False)
+    parser.add_argument('entry_point', choices=list(ep_map) + ['test'])
+    args, extra = parser.parse_known_args()
+
 
 if __name__ == '__main__':
     main()
