6e05ce8d5c48a477c8fa1d79b19f1c7e660c1719
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 15:53:48 -0400

    typo

--- armory/__main__.py
+++ armory/__main__.py
@@ -3,7 +3,7 @@ from importlib.metadata import distribution
 
 def main():
     dist = distribution('armory')
-    ep_map = {_short_name(ep.name): ep for ep in dist.entry_points if ep.group == 'console_scripts'}
+    ep_map = {ep.name: ep for ep in dist.entry_points if ep.group == 'console_scripts'}
     print(ep_map)
 
 
