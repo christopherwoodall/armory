830736bbb4630ffbde54994aa4a480a8b0bd5e06
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 11:58:54 -0400

    fix typo

--- armory/plugins/cli/__init__.py
+++ armory/plugins/cli/__init__.py
@@ -3,6 +3,11 @@ import sys
 from abc import ABC, abstractmethod
 
 class CLI(ABC):
-  def __init__(self):
-    # self.args = sys.argv[1:]
+  # def __init__(self):
+  #   self.args = sys.argv[1:]
+
+  def processCLI(self):
+    ...
+
+  def usageCLI(self):
     ...
--- armory/plugins/cli/run.py
+++ armory/plugins/cli/run.py
@@ -1,18 +1,26 @@
+'''
+
+'''
+try:
+    from armory.plugins.cli import CLI
+except Exception as e:
+    raise Exception(f"Error importing CLI module from {__file__}!")
+
+from armory.utils import debugging
 
 
-class RunCLI:
-  """run
-  --something
-  """
+class RunCLI(CLI):
+
   def __init__(self):
     print("run imported")
+
 
   def processCLI(self):
     print("run processCLI")
 
 
   def usageCsLI(self):
-    """
+    '''
     Usage:
     armory [options]
     armory ( -h | --help )
@@ -23,8 +31,8 @@ class RunCLI:
         -f=FOO, --foo=FOO     An option with an argument.
         -h, --help            Show this help message and exits.
         -V, --version         Print version and copyright information.
-    """
+    '''
     # print(self.usage.__doc__)
     # from docopt import docopt
     # docopt(self.usage.__doc__, version='armory 0.16.0')
-    ...
+    print("usageCLI")
