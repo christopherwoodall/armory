ea54c11621f590125270c787031b6e446159daf8
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 15:54:28 -0400

    compat

--- armory/__main__.py
+++ armory/__main__.py
@@ -1,8 +1,10 @@
 from importlib.metadata import distribution
 
 
+APP_NAME = 'armory'
+
 def main():
-    dist = distribution('armory')
+    dist = distribution(APP_NAME)
     ep_map = {ep.name: ep for ep in dist.entry_points if ep.group == 'console_scripts'}
     print(ep_map)
 
