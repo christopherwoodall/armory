d6db69b8887728e1e93508b31640d050bc603e13
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 08:25:03 -0400

    update venv

--- tools/bin/hacking.setup.sh
+++ tools/bin/hacking.setup.sh
@@ -10,22 +10,21 @@ BDIR="$( cd -P "$( dirname "$SOURCE" )/../.." && pwd )"
 pushd $BDIR
 
 # Setup venv
-pip install venv
-python -m venv venv
+pip3 install venv
+python3 -m venv venv
 source venv/bin/activate
 
 # pip install
 pip install -e .
-pip install -r requirements.txt
 
 # Get the VCS version
 TAG=`armory --version`
 
 # Retag images
 DOCKER_IMAGE=`docker images --all --format "{{.ID}}:{{.Repository}}:{{.Tag}}" | grep twosixarmory`
-IMAGE_IDS=`echo $DOCKER_IMAGE | cut -d: -f1`
+IMAGE_IDS=`echo $DOCKER_IMAGE    | cut -d: -f1`
 IMAGE_NAME=`echo ${DOCKER_IMAGE} | cut -d: -f2 | tr -d ' '`
-IMAGE_TAG=`echo ${DOCKER_IMAGE} | cut -d: -f3 | tr -d ' '`
+IMAGE_TAG=`echo ${DOCKER_IMAGE}  | cut -d: -f3 | tr -d ' '`
 
 echo "Retagging ${IMAGE_IDS} to ${IMAGE_NAME}:${TAG}"
 docker tag ${IMAGE_IDS} ${IMAGE_NAME}:${TAG}
