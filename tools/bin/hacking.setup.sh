e58f085cbc0740141729b97a59912189cc1f68c6
christopherwoodall <woodall.christopher@gmail.com>
2022-09-20 12:43:17 -0400

    update hacking

--- tools/bin/hacking.setup.sh
+++ tools/bin/hacking.setup.sh
@@ -2,6 +2,8 @@
 set -e
 
 PROJECT="armory"
+LATEST_RELEASE="twosixarmory/pytorch:0.14.6"
+
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
@@ -21,7 +23,7 @@ pip install --force-reinstall --no-compile --progress-bar="emoji" --editable '.[
 TAG=`armory --version`
 
 # Retag images
-DOCKER_IMAGE=`docker images --all --format "{{.ID}}:{{.Repository}}:{{.Tag}}" | grep twosixarmory`
+DOCKER_IMAGE=`docker images 'twosixarmory/*' --all --format "{{.ID}}:{{.Repository}}:{{.Tag}}" | head -n 1`
 IMAGE_IDS=`echo $DOCKER_IMAGE    | cut -d: -f1`
 IMAGE_NAME=`echo ${DOCKER_IMAGE} | cut -d: -f2 | tr -d ' '`
 IMAGE_TAG=`echo ${DOCKER_IMAGE}  | cut -d: -f3 | tr -d ' '`
@@ -29,5 +31,9 @@ IMAGE_TAG=`echo ${DOCKER_IMAGE}  | cut -d: -f3 | tr -d ' '`
 echo "Retagging ${IMAGE_IDS} to ${IMAGE_NAME}:${TAG}"
 docker tag ${IMAGE_IDS} ${IMAGE_NAME}:${TAG}
 
+# Optionally save the image
+# docker save `docker images 'twosixarmory/*' --format '{{.ID}}' | head -n 1` -o twosixarmory.tar
+# Add load it
+# docker load < twosixarmory.tar
 
 popd
