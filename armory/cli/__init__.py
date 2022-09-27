c97a3b9119b40272b5ae959cc801aba1b36f180c
christopherwoodall <christopher.woodall@twosixtech.com>
2022-09-27 12:28:36 -0400

    Add argcomplete

--- armory/cli/__init__.py
+++ armory/cli/__init__.py
@@ -1,9 +1,11 @@
 # import os
 import sys
 import argparse
+import importlib
 
-from pathlib import Path
 from abc import ABC, abstractmethod
+from inspect import getmembers, isclass, isfunction
+from pathlib import Path
 
 try:
   # https://github.com/kislyuk/argcomplete
@@ -23,10 +25,9 @@ from armory.cli.parsing import helpers
 
 # if sys.version_info < (3, 7):
 #     raise SystemExit(
-#         'ERROR: Ansible requires Python 3.9 or newer on the controller. '
+#         'ERROR: Armory requires Python 3.7 or newer. '
 #         'Current version: %s' % ''.join(sys.version.splitlines())
 #     )
-
 
 
 class CLI(ABC):
@@ -55,43 +56,21 @@ class CLI(ABC):
     self.callback = callback
 
 
-  def config(self, positional=None, flags=None, func=None):
-    parser = self.parser = argparse.ArgumentParser(prog=self.name, description=self.description)
-
-    if func:
-      parser.set_defaults(func=init)
-
-    if positional:
-      for position in positional:
-        subparsers = parser.add_subparsers(**position)
-
-    if flags:
-      for args, kwargs in flags:
-        parser.add_argument(*args, **kwargs)
-
-    #if HAS_ARGCOMPLETE:
-    #  argcomplete.autocomplete(self.parser)
-
-    self.args = self.parser.parse_args(self.args)
-
-
-
-
   @classmethod
-  def init(cls, args=None):
-    exit_code = 0
+  def init(cls, args=None, exit_code=0):
+    if args is None:
+      args = sys.argv
     try:
-      if args is None:
-        args = sys.argv
-
       cli   = cls(args)
       setup = cli.setup()
-      # exit_code = cli.run()
-
+      exit_code = cli.run()
     except KeyboardInterrupt:
       # log.warn("Execution interrupted(KeyboardInterrupt)")
       exit_code = 1
-
+    except Exception as e:
+      # log.error(e)
+      # TODO: Show stacktrace(in debug mode), start `pdb`, and enter post mortem.
+      exit_code = 1
     sys.exit(exit_code)
 
 
@@ -104,7 +83,37 @@ class CLI(ABC):
   def run():
     raise NotImplementedError("Method not implemented!")
 
+
   @abstractmethod
   def usage():
     raise NotImplementedError("Method not implemented!")
+
+
+  def config(self, positional=None, flags=None, func=None):
+    parser = self.parser = argparse.ArgumentParser(prog=self.name, description=self.description)
+    if func:
+      parser.set_defaults(func=init)
+    if positional:
+      for position in positional:
+        subparsers = parser.add_subparsers(**position)
+    if flags:
+      for args, kwargs in flags:
+        parser.add_argument(*args, **kwargs)
+    if HAS_ARGCOMPLETE:
+      argcomplete.autocomplete(self.parser)
+    self.args = self.parser.parse_args(self.args)
+
+
+  def module_loader(self, module_name: str, filepath: Path):
+  path   = str(filepath.absolute())
+  loader = importlib._bootstrap_external.SourceFileLoader(filepath.stem, path)
+  spec   = importlib.util.spec_from_file_location(filepath.stem, path, loader=loader)
+  try:
+    module = getattr(importlib._bootstrap._load(spec), module_name, False)
+    if module and isclass(module):
+      return module
+    else:
+      raise Exception(f"Error importing {module_name} module from {filepath}!")
+  except:
+    raise ImportError(path, sys.exc_info())
 
