a5b1e212e1b8ece5f81fb645ffc0ca460fb8740a
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-11 13:46:03 -0400

    Merge pytest params

--- tests/end_to_end/test_scenario_runner.py
+++ tests/end_to_end/test_scenario_runner.py
@@ -61,11 +61,11 @@ class TestScenarios(unittest.TestCase):
 
 
             for scenario in scenario_path:
-                armory_flags = ["--no-docker", "--check", "--no-gpu"]
 
                 if scenario not in block_list:
                     # scenario_log_path, scenario_log_data = self.run_scenario(scenario)
-                    run(armory_flags.insert(0, scenario.as_posix()), "armory", None)
+                    armory_flags = [scenario.as_posix(), "--no-docker", "--check", "--no-gpu"]
+                    run(armory_flags, "armory", None)
                     out, err = capsys.readouterr()
                     print(out)
                     print(err)
