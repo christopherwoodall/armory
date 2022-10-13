e96a79f1f938a7180938766d1d32973ff1b2ae6d
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-13 16:59:00 -0400

    Dead code

--- armory/docker/images.py
+++ armory/docker/images.py
@@ -1,20 +1,15 @@
 """
 Enables programmatic accessing of most recent docker images
 """
-# import docker
-# import docker.errors
-# import requests
-
-import armory
-from armory.utils import version
 from armory.docker import ImageMapper
 from armory.logs import log, is_progress
+from armory.utils import version
 
 
 class ImageMap(ImageMapper):
     repo = "twosixarmory"
     image = "fortress"
-    tag = version.to_docker_tag(armory.__version__)
+    tag = version.to_docker_tag(version.get_version())
     arsenal = ("pytorch", "pytorch-deepspeech", "tf2", "carla-mot", "fortress")
 
 
@@ -23,9 +18,6 @@ def ensure_image_present(image_name: str) -> str:
     If image_name is available, return it. Otherwise, pull it from dockerhub.
     """
     log.trace(f"ensure_image_present {image_name}")
-    # docker_client = docker.from_env(version="auto")
-
-    print(f"Checking for {image_name}...")
     image_name=ImageMap().resolve(image_name)
 
     return image_name
