37eef5a139393de24f6cc9f47f9003dd0331f0e8
Christopher Woodall <woodall.christopher@gmail.com>
2022-11-01 15:44:09 -0400

    cleanup

--- tools/datasets/entrypoint.sh
+++ tools/datasets/entrypoint.sh
@@ -8,6 +8,40 @@ cleanup() {
 }
 
 
+DATASET_NAME="${1:-mnist}"  # Dataset name
+SKIP_LFS="${2:-1}"          # Clone without large files
+OUTPUT_DIR="${3:-/tmp/mnist}"  # Output directory
+
+PROFILE_ROOT="/workspace"
+
+# pip install --upgrade pip
+# pip install -e '.[datasets]'
+
+pushd "${PROFILE_ROOT}" > /dev/null || exit 1
+  mkdir -p datasets/${DATASET_NAME}
+  pushd datasets/${DATASET_NAME}
+    if [ ! -d .git ]; then
+      # git lfs install
+      GIT_LFS_SKIP_SMUDGE="${SKIP_LFS}" git clone https://huggingface.co/datasets/${DATASET_NAME} .
+    fi
+    git pull
+
+#     # echo "Converting ${DATASET_NAME} to TFDS format"
+    mkdir -p ./data
+#     # Patch the file
+#     sed -i 's/import datasets/import tensorflow_datasets as datasets/g' mnist.py
+#     sed -i 's/from datasets.tasks import ImageClassification/from datasets import image_classification/g' mnist.py
+#     sed -i 's/ImageClassification/image_classification/g' mnist.py
+
+#     # Tensorflow Dataset Directory
+#     TFDS_DATA_DIR="`pwd`/data" tfds build \
+#       --pdb \
+#       --overwrite
+#       # --data_dir="${PROFILE_ROOT}/datasets/${DATASET_NAME}/data"
+
+  popd
+popd > /dev/null
+
 
 conda init bash
 /bin/bash
--- tools/huggingface2tfds.sh
+++ tools/huggingface2tfds.sh
@@ -9,9 +9,13 @@ trap cleanup SIGINT SIGTERM ERR EXIT
 
 cleanup() {
   trap - SIGINT SIGTERM ERR EXIT
-  # docker-compose down
-  # rm -rf tmp
+  COMPOSE_COMMAND="docker-compose"
+  if ! [ -x "$(command -v docker-compose)" ]; then
+    COMPOSE_COMMAND="docker compose"
+  fi
+  $COMPOSE_COMMAND down
 }
+
 
 DATASET_NAME="${1:-mnist}"  # Dataset name
 SKIP_LFS="${2:-1}"          # Clone without large files
@@ -20,51 +24,49 @@ SKIP_LFS="${2:-1}"          # Clone without large files
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 # TODO: Add install prefix; e.g. `/opt/armory`
-PROFILE_ROOT="${PROJECT_ROOT}/profile"
+PROFILE_ROOT="${PROJECT_ROOT}/workspace"
 
 
 # HOST_DOCKER_VERSION=`docker version --format '{{.Server.Version}}'`
-# COMPOSE_COMMAND="docker-compose"
-# if ! [ -x "$(command -v docker-compose)" ]; then
-#   COMPOSE_COMMAND="docker compose"
-# fi
+COMPOSE_COMMAND="docker-compose"
+if ! [ -x "$(command -v docker-compose)" ]; then
+  COMPOSE_COMMAND="docker compose"
+fi
 
 pushd "${PROJECT_ROOT}" > /dev/null || exit 1
   if [ ! -d "${PROFILE_ROOT}" ]; then
       mkdir -p "${PROFILE_ROOT}/"{datasets,models,tmp,workspace}
   fi
 
-  if [ ! -d "${PROFILE_ROOT}/workspace/jupyter/notebooks" ]; then
-    mkdir -p "${PROFILE_ROOT}/workspace/"{scenarios,tutorials,jupyter}
-    mkdir -p "${PROFILE_ROOT}/workspace/jupyter/notebooks"
-    mv scenario_configs "${PROFILE_ROOT}/workspace/scenarios"
-    mv tutorials "${PROFILE_ROOT}/workspace/tutorials"
-    mv notebooks "${PROFILE_ROOT}/workspace/jupyter/notebooks"
-  fi
+#   if [ ! -d "${PROFILE_ROOT}/workspace/jupyter/notebooks" ]; then
+#     mkdir -p "${PROFILE_ROOT}/workspace/"{scenarios,tutorials,jupyter}
+#     mkdir -p "${PROFILE_ROOT}/workspace/jupyter/notebooks"
+#     mv scenario_configs "${PROFILE_ROOT}/workspace/scenarios"
+#     mv tutorials "${PROFILE_ROOT}/workspace/tutorials"
+#     mv notebooks "${PROFILE_ROOT}/workspace/jupyter/notebooks"
+#   fi
 popd > /dev/null
 
 
-pushd "${PROFILE_ROOT}" > /dev/null || exit 1
-  mkdir -p datasets/${DATASET_NAME}
-  pushd datasets/${DATASET_NAME}
-    if [ ! -d .git ]; then
-      # git lfs install
-      GIT_LFS_SKIP_SMUDGE="${SKIP_LFS}" git clone https://huggingface.co/datasets/${DATASET_NAME} .
-    fi
-    git pull
+$COMPOSE_COMMAND run --entrypoint="/bin/bash -c /tmp/entrypoint.sh" --workdir="/workspace" armory-datasets
 
-    # echo "Converting ${DATASET_NAME} to TFDS format"
-    mkdir -p ./data
-    # Patch the file
-    sed -i 's/import datasets/import tensorflow_datasets as datasets/g' mnist.py
-    sed -i 's/from datasets.tasks import ImageClassification/from datasets import image_classification/g' mnist.py
-    sed -i 's/ImageClassification/image_classification/g' mnist.py
 
-    # Tensorflow Dataset Directory
-    TFDS_DATA_DIR="`pwd`/data" tfds build \
-      --pdb \
-      --overwrite
-      # --data_dir="${PROFILE_ROOT}/datasets/${DATASET_NAME}/data"
+######
+# NOTES
+#
+# docker-compose --file docker-compose.yml up -d --build armory-datasets
+# docker-compose exec armory-datasets /bin/bash
+#
+# docker compose run --entrypoint="/bin/bash -c /tmp/entrypoint.sh" --workdir="/workspace" armory-datasets
 
-  popd
-popd > /dev/null
+# docker-compose up --detach armory-launcher
+
+# docker-compose exec -T \
+#   --workdir="/workspace" \
+#   armory-launcher \
+#     /bin/bash -c /tmp/armory-shell.sh
+
+# docker-compose exec \
+#   --workdir="/workspace" \
+#   armory-launcher \
+#     /bin/bash -c tmux new-session -d -s armory; tmux attach-session -t armory
