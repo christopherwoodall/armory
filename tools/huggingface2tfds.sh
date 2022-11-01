d43062e3e2a69f6d001ed65026d5671e84e262c1
Christopher Woodall <woodall.christopher@gmail.com>
2022-11-01 15:16:06 -0400

    add cleanup

--- tools/huggingface2tfds.sh
+++ tools/huggingface2tfds.sh
@@ -5,15 +5,29 @@
 #   $ ./huggingface2tfds.sh <dataset_name> <dataset_version> <output_dir>
 # EXAMPLE:
 #   $ ./huggingface2tfds.sh glue/mrpc 1.0 /tmp/mrpc
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # docker-compose down
+  # rm -rf tmp
+}
 
 DATASET_NAME="${1:-mnist}"  # Dataset name
 SKIP_LFS="${2:-1}"          # Clone without large files
 
 
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 # TODO: Add install prefix; e.g. `/opt/armory`
 PROFILE_ROOT="${PROJECT_ROOT}/profile"
 
+
+# HOST_DOCKER_VERSION=`docker version --format '{{.Server.Version}}'`
+# COMPOSE_COMMAND="docker-compose"
+# if ! [ -x "$(command -v docker-compose)" ]; then
+#   COMPOSE_COMMAND="docker compose"
+# fi
 
 pushd "${PROJECT_ROOT}" > /dev/null || exit 1
   if [ ! -d "${PROFILE_ROOT}" ]; then
